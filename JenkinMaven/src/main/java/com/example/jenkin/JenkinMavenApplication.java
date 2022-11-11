8cebfaa33e2872fc2d19ed7e24f405248324f039
nirmit46 <ndsurve@acpce.ac.in>
2022-11-11 20:50:09 +0530

    initial commit

---
+++ JenkinMaven/src/main/java/com/example/jenkin/JenkinMavenApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinMavenApplication.class, args);
+		System.out.println("Hello Nirmit");
+	}
+
+}
